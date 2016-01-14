b53037c780dbda2f93fb222e7f4adc319f00e8cc
Josua Schmid <josua.schmid@renuo.ch>
2016-01-14 23:13:17 +0100

    make list doubly linked

--- lib/counting_list.rb
+++ lib/counting_list.rb
@@ -1,10 +1,10 @@
-Item = Struct.new(:value, :next)
+Item = Struct.new(:value, :previous, :next)
 
 class CountingList
   def initialize(*items)
     @head = nil
     items.reverse_each do |value|
-      current = Item.new(value, @head)
+      current = Item.new(value, nil, @head)
       @head = current
     end
     @hop_count = 0
@@ -12,12 +12,11 @@ class CountingList
 
   def retrieve(value, &optimize)
     current = @head
-    while current.next
+    while current
       if current.value == value
         optimize.call(@head, current) if block_given?
         return current.value
       end
-
       current = current.next
       @hop_count += 1
     end
@@ -25,5 +24,15 @@ class CountingList
 
   def hop_count
     @hop_count
+  end
+
+  def to_a
+    result = []
+    current = @head
+    while current
+      result << current.value
+      current = current.next
+    end
+    result
   end
 end
--- spec/counting_list_spec.rb
+++ spec/counting_list_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe CountingList do
   subject(:list) { described_class.new('item1', 'item2', 'item3', 'item4') }
 
   it 'can retrieve an item which is present' do
+    expect(list.retrieve('item4')).to eql('item4')
+    expect(list.retrieve('item1')).to eql('item1')
     expect(list.retrieve('item2')).to eql('item2')
+    expect(list.retrieve('item3')).to eql('item3')
   end
 
   it 'cannot retrieve an item which is not present' do
@@ -14,5 +17,9 @@ RSpec.describe CountingList do
   it 'can count retrieve hops' do
     expect { list.retrieve('item2') }.to change { list.hop_count }.by(1)
     expect { list.retrieve('item4') }.to change { list.hop_count }.by(3)
+  end
+
+  it 'can convert the linked list to an array' do
+    expect(list.to_a).to eql(['item1', 'item2', 'item3', 'item4'])
   end
 end
