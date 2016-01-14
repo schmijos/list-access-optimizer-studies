6a9a383bb4fe3361d4c0db3a2abcebf15856d6af
Josua Schmid <josua.schmid@renuo.ch>
2016-01-14 22:14:23 +0100

    introduce a linked list

--- lib/counting_list.rb
+++ lib/counting_list.rb
@@ -1,19 +1,24 @@
+Item = Struct.new(:value, :next)
+
 class CountingList
   def initialize(*items)
-    @list = items
+    last_current = nil
+    items.reverse_each do |value|
+      current = Item.new(value, last_current)
+      last_current = current
+    end
+    @head = last_current
     @hop_count = 0
   end
 
   def retrieve(value)
-    position = @list.index(value)
-    if position
-      @hop_count += position
-      @list[position]
-    end
-  end
+    current = @head
+    while current.next
+      return current.value if current.value == value
 
-  def length
-    @list.length
+      current = current.next
+      @hop_count += 1
+    end
   end
 
   def hop_count
